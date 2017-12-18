0d51c134b02a7958ddc850fd4d97b2b51959f34f
railson <railson@CPD02.distribuidora.ba>
2017-12-18 10:42:31 -0300

    teste

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Fetch;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.casadocodigo.loja.models.Preco;
 import br.com.casadocodigo.loja.models.Produto;
 
 @Repository
@@ -29,4 +36,5 @@ public class ProdutoDAO {
 	public Produto buscar(Integer id) {
 		return manager.createQuery("select distinct(p) from Produto p join fetch p.precos where p.id = :id",Produto.class).setParameter("id", id).getSingleResult();
 	}
+
 }
